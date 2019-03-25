8e980b6d82361fb02bf012bc9210db6457b30e75
jawad727 <jawaddhussein@gmail.com>
2019-03-25 12:49:04 -0700

    git push check

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js')
+
+const server = express();
+
+server.use(express.json())
+
+
+
+server.get('/', (req, res) => {
+    res.send("it works")
+})
+
+
+
+server.post('/api/users', (req, res) => {
+
+    const userInfo = req.body;
+
+    db.users
+    .add(userInfo)
+    .then(user => {
+        res.status(201).json(user);
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'error creating users' })
+    })
+})
+
+
+
+server.listen(4000, () => {
+    console.log('\n *** server running on http://localhost4000 &&&\n')
+});
